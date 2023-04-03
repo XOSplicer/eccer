68f55a650ef5406a008de8f3d842fdfaf4067c15
Felix Stegmaier <stegmaier.felix@gmail.com>
2023-04-03 19:57:08 +0200

    Better logging for worker

--- src/worker.rs
+++ src/worker.rs
@@ -1,6 +1,9 @@
 use env_logger::Env;
 use futures_util::StreamExt;
+use std::convert::TryInto;
+use std::time::{Duration, Instant};
 use structopt::StructOpt;
+use surf::{Client, Config};
 use tokio;
 
 use crate::{db, dispatch, opt, queue};
@@ -9,13 +12,29 @@ pub async fn run_worker(_opt: opt::Opt, mut db: db::Db, queue: queue::Queue) -> 
     log::info!("Running eccer worker");
     let input = queue.subscribe_ping().await?;
     futures_util::pin_mut!(input);
+    let client: Client = Config::new()
+        .set_timeout(Some(Duration::from_secs(5)))
+        .try_into()?;
     while let Some(key) = input.next().await {
         let key = key?;
-        log::info!("got {}", &key);
-        let url = db.get_endpoint_url(key).await?;
-        log::info!("url {}", &url);
-        let res_status = surf::get(&url).await.map(|res| res.status());
-        log::info!("res_status {:?}", &res_status);
+        let url = db.get_endpoint_url(key.clone()).await?;
+        log::info!("GET ping key={} url={}", &key, &url);
+        let start = Instant::now();
+        let res_status = client.get(&url).await.map(|res| res.status());
+        let is_success = match res_status {
+            Err(_) => false,
+            Ok(status) => status.is_success(),
+        };
+        let duration = Instant::now().duration_since(start);
+        let duration_ms = duration.as_micros() as f64 / 1000.0;
+        log::info!(
+            "GET repsonse key={} url={} response={:?} success={} duration={:.2}ms",
+            &key,
+            &url,
+            res_status,
+            is_success,
+            duration_ms
+        );
     }
     Ok(())
 }
